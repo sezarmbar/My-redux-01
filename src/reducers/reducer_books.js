157740002db004feb11c7114c922eb96df04925e
sezarmbar <sezar_mbar@yahoo.com>
2016-12-20 16:59:02 +0100

    reducer undfinde

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title:'javascript Good Parts'},
+        {title:'Java All Waht You Need'},
+        {title:'Grails'},
+        {title:'Groovy'}
+    ]
+}
